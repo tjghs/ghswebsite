a291aed8b659d9a1d9bcbe5dee00f4fb00df89cc
William Zhang <willzhang05@gmail.com>
2016-09-08 18:16:41 -0400

    Added static urls

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, send_from_directory
 from flask.ext.sqlalchemy import SQLAlchemy
 
 
@@ -12,13 +12,25 @@ db = SQLAlchemy(app)
 print(os.environ['APP_SETTINGS'])
 
 
-from models import Hours
-
-
 @app.route("/")
-def hello():
-    return "Hello World"
+def index():
+    return render_template("main.html")    
 
+@app.route("/css/<path:path>")
+def send_css(path):
+    return send_from_directory('static/css', path)
+
+@app.route("/scripts/<path:path>")
+def send_js(path):
+    return send_from_directory('static/scripts', path)
+
+@app.route("/icons/<path:path>")
+def send_icons(path):
+    return send_from_directory('static/icons', path)
+
+@app.route("/fonts/<path:path>")
+def send_fonts(path):
+    return send_from_directory('static/fonts', path)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
