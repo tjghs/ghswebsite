818ed93d2ea88a34f42d43a9f28df2d7135f7852
William Zhang <willzhang05@gmail.com>
2016-09-08 16:43:26 -0400

    Finished migrations

--- models.py
+++ models.py
@@ -5,9 +5,11 @@ class Hours(db.Model):
     __tablename__ = 'hours'
     
     id = db.Column(db.Integer, primary_key=True)
-    hours = db.Column(db.Integer, primary_key=True)
+    desc = db.Column(db.String())
+    hours = db.Column(db.Integer, primary_key=False)
     
-    def __init__(self, hours):
+    def __init__(self, desc, hours):
+        self.desc = desc
         self.hours = hours
 
     def __repr__(self):
