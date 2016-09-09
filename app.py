fd0eb3382731651f26ad8b42ed83bebd8031be60
William Zhang <willzhang05@gmail.com>
2016-09-09 11:12:53 -0400

    Added a couple new fields that Naitian thought would be a good idea

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+import models
 
 print(os.environ['APP_SETTINGS'])
 
--- models.py
+++ models.py
@@ -5,12 +5,18 @@ class Hours(db.Model):
     __tablename__ = 'hours'
     
     id = db.Column(db.Integer, primary_key=True)
-    desc = db.Column(db.String())
-    hours = db.Column(db.Integer, primary_key=False)
-    
-    def __init__(self, desc, hours):
+    admin = db.Column(db.Boolean, primary_key=True)
+    desc = db.Column(db.String(), primary_key=True)
+    hours = db.Column(db.Integer)
+    date = db.Column(db.Date, primary_key=True)
+    item = db.Column(db.Boolean, primary_key=True)
+
+    def __init__(self, admin, desc, hours, date, item):
         self.desc = desc
+        self.admin = admin
         self.hours = hours
+        self.date = date
+        self.item = item
 
     def __repr__(self):
         return '<id{}>'.format(self.id)
