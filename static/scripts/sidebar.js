37eee83c990865d6f98323ac9197d9aed4679751
William Zhang <willzhang05@gmail.com>
2016-11-07 14:01:12 -0500

    Fix sidebar and scrolling

--- static/scripts/sidebar.js
+++ static/scripts/sidebar.js
@@ -4,17 +4,18 @@ var mb = document.querySelector("#nav-button"),
 
 mb.addEventListener("click", function(){toggleSidebar(true)}, false);
 wr.addEventListener("click", function(){toggleSidebar(false)}, false);
-/*mb.addEventListener("touchend", function(){toggleSidebar(true)}, false);
-wr.addEventListener("touchend", function(){toggleSidebar(false)}, false);
-*/
+mb.addEventListener("touchleave", function(){toggleSidebar(true)}, false);
+wr.addEventListener("touchleave", function(){toggleSidebar(false)}, false);
 sb.style.disabled = true;
 
+
 function toggleSidebar(toggleOpenAllowed) {
-    if((sb.style.disabled && toggleOpenAllowed) || (sb.style.right == "" && toggleOpenAllowed)) { //toggle
+    if((sb.style.right == "" || sb.style.right == "-200px") && toggleOpenAllowed) {
         sb.style.disabled = false;
         sb.style.right = "0px";
-    } else { //untoggle
-        sb.style.right = "-200px";
+    } else {
         sb.style.disabled = true;
+        sb.style.right = "-200px";
     }
 }
+
