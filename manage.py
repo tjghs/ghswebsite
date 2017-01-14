e519f7b5ce530fe9e516cea5887395ec104272b4
William Zhang <willzhang05@gmail.com>
2017-01-14 18:57:06 -0500

    fix outdated imports in manage

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import os
-from flask.ext.script import Manager
-from flask.ext.migrate import Migrate, MigrateCommand
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 from app import app, db
 
