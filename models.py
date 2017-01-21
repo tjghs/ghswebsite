8bd936855b38bc7398296b48e647e9f62c6af8d5
William Zhang <willzhang05@gmail.com>
2017-01-20 21:40:22 -0500

    Fix margin issue and update models to include announcements table

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ class Hours(db.Model):
     desc = db.Column(db.String(), primary_key=True)
     hours = db.Column(db.Integer)
     date = db.Column(db.Date, primary_key=True)
-    item = db.Column(db.Boolean, primary_key=True)
+    item = db.Column(db.Boolean, primary_key=True) #if item that can be counted as hour
 
     def __init__(self, admin, desc, hours, date, item):
         self.desc = desc
@@ -19,6 +19,25 @@ class Hours(db.Model):
         self.hours = hours
         self.date = date
         self.item = item
+
+    def __repr__(self):
+        return "<id{}>".format(self.id)
+
+
+class Announcements(db.Model):
+    __tablename__ = "announcements"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(), primary_key=True)
+    tag = db.Column(db.String(), primary_key=True)
+    desc = db.Column(db.String())
+    date = db.Column(db.Date, primary_key=True)
+
+    def __init__(self, title, tag, desc, date):
+        self.title = title
+        self.tag = tag
+        self.desc = desc
+        self.date = date
 
     def __repr__(self):
         return "<id{}>".format(self.id)
