cb6aed6ee6884ac3c8b9625de7d9ade6c77f72ef
William Zhang <willzhang05@gmail.com>
2017-05-18 21:10:07 -0400

    actually fix run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-export FLASK_APP=/web/activities/public/ghswebsite/app.py
+export FLASK_APP=/web/activities/ghs/public/ghswebsite/app.py
 export APP_SETTINGS="config.ProductionConfig"
 /web/activities/ghs/public/env/bin/flask run -p $PORT
