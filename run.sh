4f14b91a758df5b8fe98c47e54e63fefdcf7705b
William Zhang <willzhang05@gmail.com>
2017-05-18 21:08:45 -0400

    remove prodenv and make everything contained in run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-source /web/activities/ghs/public/.prodenv
+export FLASK_APP=/web/activities/public/ghswebsite/app.py
+export APP_SETTINGS="config.ProductionConfig"
 /web/activities/ghs/public/env/bin/flask run -p $PORT
