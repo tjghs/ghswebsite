537a56d6d19bb8038c1b5b3d3194d19e7ecbdad9
William Zhang <willzhang05@gmail.com>
2017-09-13 17:14:41 -0400

    more cleanup

--- ghswebsite/ghswebsite/example_settings.py
+++ ghswebsite/ghswebsite/example_settings.py
@@ -30,9 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-#    'search.apps.SearchConfig',
     'django.contrib.admin',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
