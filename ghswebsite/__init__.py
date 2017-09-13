d8408f5feaabd880c80e6d11fffcd078d1fd785c
William Zhang <willzhang05@gmail.com>
2017-09-13 16:56:04 -0400

    initial django stuff

--- ghswebsite/__init__.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-from flask import Flask, redirect, session, url_for, render_template, request, send_from_directory
-from flask_sqlalchemy import SQLAlchemy
-
-from ghswebsite.app import *
-from ghswebsite.models import *
-from ghswebsite.forms import *
-from ghswebsite.views import *
